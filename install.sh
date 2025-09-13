fb3f908c46b90967b3ad3318f4069956d9042fc3
K_atc <shiftx1026@gmail.com>
2025-09-13 14:20:57 +0900

    Complete? install.sh

--- install.sh
+++ install.sh
@@ -2,22 +2,25 @@
 
 artifact_dir=$(cd $(dirname $0)/artifact; pwd)
 
-# sudo apt update
+sudo apt update -y
+sudo apt install -y snapd
+
 sudo apt install -y python3 python3-pip
 
 # ### Install Apache Airflow
 # pip3 install apache-airflow==2.7.0
 
 ### Install Docker
+sudo snap install docker
 
 ### Install Scala CLI
-# TODO: 
+curl -sSLf https://scala-cli.virtuslab.org/get | sh
 
 ### Install PolyTracker
 cd ${artifact_dir}/polytracker
-# TODO: 
+pip install .
 
-
+### Install docker container helpers
 cd ${artifact_dir}/work-desk
 ./setup.sc polytracker
 ./setup.sc polytracker.slim
