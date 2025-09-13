c00d0e12d176d2e1005daabcb40b6911404c54a1
K_atc <shiftx1026@gmail.com>
2025-09-13 06:17:02 +0000

    Failed to run install.sh on Ubuntu 24.04

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -eux
 
 sudo apt update -y
 sudo apt install -y snapd
+export PATH=$PATH:$HOME/.local/bin
 
 sudo apt install -y python3 python3-pip
 
@@ -22,6 +23,16 @@ if ! id -nG "$USER" | grep -qw "docker"; then
     sudo usermod -aG docker "$USER"
 fi
 
+# ### Activate proper Python environment for Ubuntu 24.04 (Not works)
+# sudo apt install software-properties-common -y
+# sudo add-apt-repository ppa:deadsnakes/ppa -y
+# sudo apt update
+# sudo apt install python3.10 python3.10-venv python3.10-dev python3.10-distutils -y
+# curl -sSL https://install.python-poetry.org | python3 -
+# cd ${artifact_dir}
+# poetry env use python3.10
+# poetry env activate
+
 ### Install Scala CLI
 curl -fL https://github.com/Virtuslab/scala-cli/releases/latest/download/scala-cli-x86_64-pc-linux.gz | gzip -d > scala-cli
 chmod +x scala-cli
@@ -31,9 +42,8 @@ sudo mv scala-cli /usr/local/bin/scala-cli
 cd ${artifact_dir}/polytracker
 pip3 install -e . --break-system-packages
 
-### Install docker container helpers
+### Build docker image for PolyTracker
 cd ${artifact_dir}/work-desk
-export PATH=$PATH:${HOME}/.local/bin/
 ./setup.sc polytracker
 ./setup.sc polytracker.slim
 
