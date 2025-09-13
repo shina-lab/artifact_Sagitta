cd26deb9adfb20c22943995ed44bef66f28d4596
K_atc <shiftx1026@gmail.com>
2025-09-13 08:24:49 +0000

    Remove polytracker

--- artifact/polytracker/build_in_docker/scripts/compile_mupdf.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-mkdir -p /sources/bin
-cd /sources/mupdf/build/debug
-${CC} --lower-bitcode -i mutool.bc -o mutool_track "${@:2}" --libs libmupdf.a m pthread
-cp "mutool_track" "/sources/bin/$1"
-cp "mutool_track.bc" "/sources/bin/$1.bc"
-cp "mutool_track.o" "/sources/bin/$1.o"
